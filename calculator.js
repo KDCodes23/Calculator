2c174ae2bba5ad23d8408534bd7fe27a53dcca91
Kanishka Dutta <kdutta2017@outlook.com>
2023-09-03 20:28:27 -0400

    Some terminology changes and javascript
    I made some changes to the terminology for the numbers buttons and added the required JavaScript.

--- calculator.js
+++ calculator.js
@@ -1,1 +1,32 @@
-let btn = document.querySelectorAll(".action-button");
+let btn = document.querySelectorAll(".num-btn");
+let allBtns = document.querySelectorAll(".button");
+let resultBox = document.querySelector("#result-box");
+let clearBtn = document.querySelector('#clear');
+let total = document.querySelector('#total');
+
+let btnSpread = [...btns];
+let allBtnSpread = [...allBtns];
+
+btnSpread.forEach((button, i ) =>
+{
+    button.addEventListener("click", () => {
+        if(resultBox.innerHTML == "0")
+        {
+            resultBox.innerHTML = "";
+        }
+        let value = btns[i].innerHTML;
+        resultBox.innerHTML += value;
+    });
+});
+function evaluate(fn) {
+    return new Function('return ' + fn)();
+}
+total.addEventListener('click', ()=> {
+let allInputs = resultBox.innerHTML;
+resultBox.innerHTML = evaluate(allInputs);
+console.log(evaluate(allInputs));
+})
+clearBtn.addEventListener('click', ()=> {
+    resultBox.innerHTML = "0";
+})
+ 
